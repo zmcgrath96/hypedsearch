d99d57cc99c7b0cea1fb1472d0fe6a45904628e6
Zach <zmcgrath96@gmail.com>
2020-06-12 14:55:07 -0600

    added filtering to mzML reader. Helps with long analysis time of the real spectra

--- src/file_io/mzML.py
+++ src/file_io/mzML.py
@@ -2,12 +2,14 @@ from src.utils import file_exists
 from src.types.objects import Spectrum
 from pyteomics import mzml
 
-def read(filename: str) -> list:
+def read(filename: str, peak_filter=50) -> list:
     '''
     read an .mzML file into memory
 
     Inputs:
-        filename:   (str) path to the file to import
+        filename:       (str) path to the file to import
+    kwargs:
+        peak_filter:    (int) the top number of peaks to keep. Default=50
     Outputs:
         (list) Spectrum namedtuple instances
     '''
@@ -19,8 +21,10 @@ def read(filename: str) -> list:
         
         filecontents = mzml.read(filename)
         for content in filecontents:
+            
+
             # sort the spectra 
-            sorted_labeled_spec = sorted([(i, float(spec)) for i, spec in list(enumerate(content['m/z array']))], key=lambda x: x[1])
+            sorted_labeled_spec = sorted([(i, float(spec)) for i, spec in list(enumerate(content['m/z array']))], key=lambda x: x[1])[:peak_filter]
             sorted_keys = [x[0] for x in sorted_labeled_spec]
             sorted_spec = [x[1] for x in sorted_labeled_spec]
             sorted_abundances = [float(content['intensity array'][i]) for i in sorted_keys]
